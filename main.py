6be765894f2c12dfdb23aedfbe1365c0f943c685
Alexander Bukin <lxndrbukin@proton.me>
2025-11-17 20:43:43 +0000

    Add load_db for loading the JSON database on api start. Add save_db for saving data to the JSON database. Update route methods

--- main.py
+++ main.py
@@ -2,33 +2,64 @@ from datetime import date
 from fastapi import FastAPI, status
 from pydantic import BaseModel
 from typing import List
+import json
+import os
 
 class Task(BaseModel):
-	id: int
-	entry: str
-	priority: str
-	due: date
-
-fake_db = []
+    id: int
+    entry: str
+    priority: str
+    due: date
 
 app = FastAPI(title="TaskFlow API", description="TaskFlow")
 
+DB_PATH = "tasks.json"
+
+def load_db():
+    global cached_db
+    if not os.path.exists(DB_PATH):
+        cached_db = []
+        save_db()
+    else:
+        with open(DB_PATH, "r") as file:
+            content = file.read().strip()
+            if not content:
+                cached_db = []
+            else:
+                try:
+                    raw = json.loads(content)
+                    cached_db = [Task(**item) for item in raw]
+                except json.JSONDecodeError:
+                    cached_db = []
+                    save_db()
+
+
+load_db()
+
+def save_db():
+    with open(DB_PATH, "w") as file:
+
+        json.dump([task.dict() for task in cached_db], file, indent=2, default=str)
+
 @app.get("/")
 def home():
-	return {"message": "Welcome to the TaskFlow API"}
+    return {"message": "Welcome to the TaskFlow API"}
 
 @app.get("/tasks", response_model=List[Task])
 def read_tasks():
-	return fake_db
+    return cached_db
 
 @app.get("/tasks/{task_id}")
 def read_task(task_id: int):
-	for task in fake_db:
-		if task.id == task_id:
-			return task
-	return {"error": f"Task with ID {task_id} not found"}
+    for task in cached_db:
+        if task.id == task_id:
+            return task
+    return {"error": f"Task with ID {task_id} not found"}
 
 @app.post("/tasks", status_code=status.HTTP_201_CREATED, response_model=Task)
 def create_entry(task: Task):
-	fake_db.append(task)
-	return task
+    new_id = max([t.id for t in cached_db], default=0) + 1
+    new_task = task.model_copy(update={"id": new_id})
+    cached_db.append(new_task)
+    save_db()
+    return task
