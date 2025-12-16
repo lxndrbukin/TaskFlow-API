252d82ece900af9c72a3d78a0df63bdf79a008ae
Alexander Bukin <lxndrbukin@proton.me>
2025-12-16 20:41:08 +0000

    Update register route

--- routers/auth.py
+++ routers/auth.py
@@ -1,14 +1,32 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+import sqlite3
 from passlib.context import CryptContext
 from models import User
+from db import load_db
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@auth_router.post("/register")
-def register(user: User):
-	if len(user.password.encode("utf-8")) > 72:
+@auth_router.post("/register", status_code=status.HTTP_201_CREATED)
+def register(form: User, db: sqlite3.Connection = Depends(load_db)):
+	cursor = db.cursor()
+	cursor.execute('SELECT COUNT(*) FROM users WHERE username = ?', (form.username,))
+	row = cursor.fetchone()
+	if row[0] > 0:
+		raise HTTPException(status_code=409, detail="Username taken")
+	if len(form.password.encode("utf-8")) > 72:
 		raise HTTPException(status_code=400, detail="Password too long")
-	user.password = pwd_context.hash(user.password)
-	return user
+	form.password = pwd_context.hash(form.password)
+	insert_user = '''
+        INSERT INTO users(username, password)
+        VALUES(?, ?)
+    '''
+	values = (
+		form.username,
+		form.password
+	)
+	cursor.execute(insert_user, values)
+	db.commit()
+	return {"msg": "User registered successfully", "username": form.username}
